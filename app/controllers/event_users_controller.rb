b04d957af174258a36b908049f3be686dee78e54
purplesands <mullenjoe13@gmail.com>
2019-02-11 16:52:26 -0500

    add event to profile button added, needs update with current user functionality

--- app/controllers/event_users_controller.rb
+++ app/controllers/event_users_controller.rb
@@ -1,2 +1,15 @@
 class EventUsersController < ApplicationController
+  #
+  # def new
+  #   @event_user = EventUser.new
+  # end
+
+  def create
+    byebug
+    EventUser.create(event_id: params[:event_id], user_id: params[:user_id])
+  end
+
+
+
+
 end
