56b9eabf6e5ab4d1a02e9efe5f4f8f3b8d1d1fa5
cvolpe12 <christopher.volpe12@gmail.com>
2019-02-12 11:49:41 -0500

    user can leave comment on location show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -24,11 +24,12 @@ class ReviewsController < ApplicationController
   # POST /reviews
   # POST /reviews.json
   def create
+    byebug
     @review = Review.new(review_params)
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.html { redirect_to location_path(@review.location), notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
@@ -69,6 +70,6 @@ class ReviewsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
-      params.require(:review).permit(:content, :rating, :user, :location)
+      params.require(:review).permit(:content, :rating, :user_id, :location_id)
     end
 end
